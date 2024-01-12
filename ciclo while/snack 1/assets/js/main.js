382ac00d507f8d83c96fd9e07cb8c4cc55b59d33
GiacomoMilitello <g.militello95@gmail.com>
2024-01-12 15:55:08 +0100

    while snack 1 js

--- ciclo while/snack 1/assets/js/main.js
+++ ciclo while/snack 1/assets/js/main.js
@@ -3,3 +3,26 @@ Ciclo While
 snack 1:
 Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50
 */
+
+const buttonOk = document.getElementById("ok");
+const numberHtml = document.getElementById('number');
+const ulHtml = document.getElementById('result');
+const errorMessage = document.getElementById('error-message');
+let numbersArray = [];
+let sum = 0;
+
+buttonOk.addEventListener('click', function() {
+    const n = Number(numberHtml.value);
+    if (!isNaN(n)) {
+        numbersArray.push(n);
+        sum += n;
+        let listItem = document.createElement('li');
+        listItem.textContent = `Numero ${n} è stato aggiunto. Ora la somma è ${sum}`;
+        ulHtml.appendChild(listItem);
+    } if (sum > 50) {
+            let finalMessage = document.createElement('li');
+            finalMessage.textContent = `Con l'ultimo numero hai superato 50!!!`;
+            ulHtml.appendChild(finalMessage);
+            buttonOk.disabled = true;
+    }
+})
